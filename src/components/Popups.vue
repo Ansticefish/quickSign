4090ae8ad902269186c5e21f3a02fc631a377d23
Ansticefish <ansticefish0106@gmail.com>
2022-11-19 22:41:37 +0800

    Add pop-ups on UploadFile page

---
+++ src/components/Popups.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+div.mask
+  div.window     
+    h2 {{ caption }}
+    div.btns
+      button.btns__orange(v-if="btnCaption.length < 1" @click="closeWindow")  {{ btnCaption[1]}}
+      button.btns__blue(@click="closeWindow") {{ btnCaption[0] }}
+</template>
+
+<script>
+export default {
+  name: 'Popups',
+  props: {
+    caption: {
+      type: String,
+      required: true
+    },
+    btnCaption: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    closeWindow () {
+      if (this.caption === '檔案上傳成功，請選擇想增加簽名的檔案！'){
+        this.$emit('close-window')
+        this.$router.push('/uploadFile/old')
+      }
+      this.$emit('close-window')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/popups.scss';
+.mask {
+  @extend %mask;
+  .window {
+    @include containerStyle (43.2vw, 27.5vh, $white, none, 35px);
+    @include fontStyle ($h2, $dark-grey);
+    @include flex (center, center);
+    flex-direction: column;
+    position: relative;
+    .btns {
+      @include position (absolute, $bottom: 1vh, $right: 2vw);
+      &__orange {
+        @extend %close-btn-orange;
+      }
+      &__blue {
+        @extend %open-file-btn;
+      }
+    }
+  }
+}
+</style>
--- src/components/UploadFileNew.vue
+++ src/components/UploadFileNew.vue
@@ -1,15 +1,52 @@
 <template lang="pug">
 div.bottom
   div.upload-area
-    div.instruction
+    label.instruction(for="upload")
+      input(type="file" accept=".pdf" id="upload" @change="uploadFile")
       p.instruction__top 點擊此處上傳 或 直接拖曳檔案
       img(src="~@/assets/image/upload-pdf.png")
       p.instruction__bottom (限10MB以下PDF檔)
+  //pop-up windows
+  Popups(:caption="caption" :btn-caption="btnCaption" @close-window="closePopups" v-if="showPopup")
 </template>
 
 <script>
+import Popups from '../components/Popups.vue'
+
 export default {
-  name: 'UploadFileNew'
+  name: 'UploadFileNew', 
+  data () {
+    return {
+      caption: '',
+      btnCaption: [],
+      showPopup: false
+    }
+  },
+  components: {
+    Popups,
+  },
+  methods: {
+    uploadFile ($e) {
+      const file = $e.target.files[0]
+      console.log(file)
+      if(file.type !== "application/pdf"){
+        this.showPopups('您的檔案不是PDF檔！', ["我知道了"] )
+      } else if (file.size > 10*1024*1024 ) {
+        this.showPopups('您的檔案太大了！', ["我知道了"])
+      } else {
+        this.showPopups('檔案上傳成功，請選擇想增加簽名的檔案！', ["我知道了"])
+        // save file to localStorage & vuex
+      }
+    },
+    closePopups () {
+      this.showPopup = false
+    },
+    showPopups ( caption, btnCaption) {
+      this.caption = caption
+      this.btnCaption = btnCaption
+      this.showPopup = true
+    }
+  }
 }
 
 </script>
@@ -19,5 +56,11 @@ export default {
 @import '../assets/scss/upload.scss';
 .bottom { 
   @extend %uploadFileStyle;
+  input {
+    display: none;
+  }
+  p, img {
+    cursor: pointer;
+  }
 }
 </style>
